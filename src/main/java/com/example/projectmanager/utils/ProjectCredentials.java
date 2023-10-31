cecd126d8774c9dcbea98de7ec59f20187bacff8
marekkaczbartek <bartek.kaczmarek03@gmail.com>
2023-10-31 17:20:16 +0100

    updated project credentials do work with the service

--- src/main/java/com/example/projectmanager/utils/ProjectCredentials.java
+++ src/main/java/com/example/projectmanager/utils/ProjectCredentials.java
@@ -1,2 +1,24 @@
-package com.example.projectmanager.utils;public class ProjectCredencials {
+package com.example.projectmanager.utils;
+
+import com.example.projectmanager.entities.Developer;
+import com.example.projectmanager.entities.User;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ProjectCredentials {
+    @JsonProperty("name")
+    private String name;
+    @JsonProperty("users")
+    private HashMap<Long, Specialization> users;
 }
