bcc84f8e85ee5c5faab22f46e3bac94599107393
marekkaczbartek <bartek.kaczmarek03@gmail.com>
2023-10-31 17:19:34 +0100

    added project service and controller, and addNewProject

--- src/main/java/com/example/projectmanager/controllers/ProjectController.java
+++ src/main/java/com/example/projectmanager/controllers/ProjectController.java
@@ -1,2 +1,23 @@
-package com.example.projectmanager.controllers;public class ProjectController {
+package com.example.projectmanager.controllers;
+
+import com.example.projectmanager.services.ProjectService;
+import com.example.projectmanager.utils.ProjectCredentials;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProjectController {
+    private final ProjectService projectService;
+
+    @Autowired
+    public ProjectController(ProjectService projectService) {
+        this.projectService = projectService;
+    }
+
+    @PostMapping("/project")
+    public void addNewProject(@RequestBody ProjectCredentials projectCredentials) {
+        this.projectService.addNewProject(projectCredentials);
+    }
 }
--- src/main/java/com/example/projectmanager/services/ProjectService.java
+++ src/main/java/com/example/projectmanager/services/ProjectService.java
@@ -1,2 +1,38 @@
-package com.example.projectmanager.services;public class ProjectService {
+package com.example.projectmanager.services;
+
+import com.example.projectmanager.entities.Developer;
+import com.example.projectmanager.entities.Project;
+import com.example.projectmanager.entities.User;
+import com.example.projectmanager.repositories.DeveloperRepository;
+import com.example.projectmanager.repositories.ProjectRepository;
+import com.example.projectmanager.utils.ProjectCredentials;
+import com.example.projectmanager.utils.Specialization;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+public class ProjectService {
+    private final ProjectRepository projectRepository;
+    private final DeveloperRepository developerRepository;
+
+    @Autowired
+    public ProjectService(ProjectRepository projectRepository,
+                          DeveloperRepository developerRepository) {
+        this.projectRepository = projectRepository;
+        this.developerRepository = developerRepository;
+    }
+
+
+    public void addNewProject(ProjectCredentials projectCredentials) {
+        Project newProject = new Project(projectCredentials.getName());
+        this.projectRepository.save(newProject);
+        for (Map.Entry<Long, Specialization> entry : projectCredentials.getUsers().entrySet()) {
+            Long userId = entry.getKey();
+            Specialization specialization = entry.getValue();
+            System.out.println(newProject.getId());
+            developerRepository.save(new Developer(userId, newProject.getId(), specialization));
+        }
+    }
 }
