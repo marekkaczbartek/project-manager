eb0f5fb65c31ff55a214bcf08cb4c33f8161a0e5
marekkaczbartek <bartek.kaczmarek03@gmail.com>
2023-10-31 16:47:30 +0100

    added getUsers and addNewUser functionalities

--- src/main/java/com/example/projectmanager/controllers/UserController.java
+++ src/main/java/com/example/projectmanager/controllers/UserController.java
@@ -1,2 +1,27 @@
-package com.example.projectmanager.controllers;public class UserController {
+package com.example.projectmanager.controllers;
+
+import com.example.projectmanager.entities.User;
+import com.example.projectmanager.services.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/users")
+    public Iterable<User> getAllUsers() {
+        return this.userService.getAllUsers();
+    }
+
+    @PostMapping("/users")
+    public void addNewUser(@RequestBody User user) {
+        this.userService.addNewUser(user);
+    }
 }
--- src/main/java/com/example/projectmanager/services/UserService.java
+++ src/main/java/com/example/projectmanager/services/UserService.java
@@ -1,2 +1,23 @@
-package com.example.projectmanager.services;public class UserService {
+package com.example.projectmanager.services;
+
+import com.example.projectmanager.entities.User;
+import com.example.projectmanager.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void addNewUser(User user) {
+        this.userRepository.save(user);
+    }
+
+    public Iterable<User> getAllUsers() {
+        return this.userRepository.findAll();
+    }
 }
