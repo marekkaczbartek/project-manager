fe868cd8427c23d514bd9ab732db8e5abebe20fe
marekkaczbartek <bartek.kaczmarek03@gmail.com>
2023-11-03 12:21:10 +0100

    Revert "commit to revert"
    This reverts commit 6d1ea81e0d49c03d7191723e07e7da91709fa23d.

--- src/main/java/com/example/projectmanager/services/DeveloperService.java
+++ src/main/java/com/example/projectmanager/services/DeveloperService.java
@@ -1,27 +1,2 @@
-package com.example.projectmanager.services;
-
-import com.example.projectmanager.entities.Developer;
-import com.example.projectmanager.entities.User;
-import com.example.projectmanager.repositories.DeveloperRepository;
-import com.example.projectmanager.utils.Specialization;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-
-@Service
-public class DeveloperService {
-    private final DeveloperRepository developerRepository;
-
-    @Autowired
-    public DeveloperService(DeveloperRepository developerRepository) {
-        this.developerRepository = developerRepository;
-    }
-    public void addDeveloperToProject(Developer developer) {
-        Long projectId = developer.getProject().getId();
-        Long userId = developer.getUser().getId();
-        this.developerRepository.save(developer);
-    }
-
-
+package com.example.projectmanager.services;public class DeveloperService {
 }
--- src/main/java/com/example/projectmanager/utils/TaskCredentials.java
+++ src/main/java/com/example/projectmanager/utils/TaskCredentials.java
@@ -1,23 +1,2 @@
-package com.example.projectmanager.utils;
-
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
-public class TaskCredentials {
-    @JsonProperty("name")
-    private String name;
-    @JsonProperty("description")
-    private String description;
-    @JsonProperty("userId")
-    private Long assignedToId;
-    @JsonProperty("specialization")
-    private Specialization specialization;
+package com.example.projectmanager.utils;public class TaskCredentials {
 }
