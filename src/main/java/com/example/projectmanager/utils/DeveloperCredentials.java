0686b5ffad3c80aad11244d98b48b75ada1ef029
marekkaczbartek <bartek.kaczmarek03@gmail.com>
2023-11-03 19:58:50 +0100

    changed id to string

--- src/main/java/com/example/projectmanager/utils/DeveloperCredentials.java
+++ src/main/java/com/example/projectmanager/utils/DeveloperCredentials.java
@@ -2,10 +2,13 @@ package com.example.projectmanager.utils;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 @AllArgsConstructor
+@Getter
 public class DeveloperCredentials {
     @JsonProperty("userId")
-    private final Long userId;
+    private final String userId;
     @JsonProperty("specialization")
     private final Specialization specialization;
 }
