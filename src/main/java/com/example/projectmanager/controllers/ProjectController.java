a91b895ba0838d0907995f39bbeef23bc0258909
marekkaczbartek <bartek.kaczmarek03@gmail.com>
2023-11-03 12:13:57 +0100

    defined relations between entities

--- src/main/java/com/example/projectmanager/controllers/ProjectController.java
+++ src/main/java/com/example/projectmanager/controllers/ProjectController.java
@@ -1,11 +1,22 @@
 package com.example.projectmanager.controllers;
 
+import com.example.projectmanager.entities.Developer;
+import com.example.projectmanager.entities.Project;
+import com.example.projectmanager.entities.User;
 import com.example.projectmanager.services.ProjectService;
 import com.example.projectmanager.utils.ProjectCredentials;
+import com.example.projectmanager.utils.Specialization;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class ProjectController {
@@ -17,7 +28,18 @@ public class ProjectController {
     }
 
     @PostMapping("/project")
-    public void addNewProject(@RequestBody ProjectCredentials projectCredentials) {
-        this.projectService.addNewProject(projectCredentials);
+    public ResponseEntity<Project> addNewProject(@RequestBody ProjectCredentials projectCredentials) {
+        Project project = this.projectService.addNewProject(projectCredentials);
+        return new ResponseEntity<>(project, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/project/empty")
+    public void addEmptyProject(@RequestBody String name) {
+        this.projectService.addEmptyProject(name);
+    }
+
+    @GetMapping("/project")
+    public List<Project> getAllProjects() {
+        return this.projectService.getAllProjects();
     }
 }
