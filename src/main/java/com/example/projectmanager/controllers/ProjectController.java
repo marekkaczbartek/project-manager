7b9602de729d590f1580b3cd7e4e3fa30024567c
marekkaczbartek <bartek.kaczmarek03@gmail.com>
2023-11-06 21:20:41 +0100

    added adding new dev to project and querying users from projects

--- src/main/java/com/example/projectmanager/controllers/ProjectController.java
+++ src/main/java/com/example/projectmanager/controllers/ProjectController.java
@@ -1,16 +1,17 @@
 package com.example.projectmanager.controllers;
 
+import com.example.projectmanager.entities.Developer;
 import com.example.projectmanager.entities.Project;
+import com.example.projectmanager.entities.User;
+import com.example.projectmanager.exceptions.ProjectNotFoundException;
 import com.example.projectmanager.exceptions.UserNotFoundException;
 import com.example.projectmanager.services.ProjectService;
+import com.example.projectmanager.utils.DeveloperCredentials;
 import com.example.projectmanager.utils.ProjectCredentials;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
@@ -30,13 +31,40 @@ public class ProjectController {
             Project project = this.projectService.addNewProject(projectCredentials);
             return new ResponseEntity<>(project, HttpStatus.CREATED);
         }
-        catch (UserNotFoundException e) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "User does not exist", e);
+        catch (RuntimeException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage(), e);
         }
     }
 
     @GetMapping("/project")
     public List<Project> getAllProjects() {
         return this.projectService.getAllProjects();
+    }
+
+    @GetMapping("/project/{id}")
+    public Project getProjectById(@PathVariable(name = "id") Long id) {
+        try {
+            return this.projectService.getProjectById(id);
+        }
+        catch(ProjectNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage(),  e);
+        }
+    }
+
+    @GetMapping("/project/{id}/users")
+    public List<User> getUsersInProject(@PathVariable(value = "id") Long projectId) {
+        return this.projectService.getUsersInProject(projectId);
+    }
+
+    @PostMapping("/project/{id}/user")
+    public ResponseEntity<Developer> addDeveloperToProject(@PathVariable(value = "id") Long projectId,
+                                                           @RequestBody DeveloperCredentials developerCredentials) {
+        try {
+            Developer developer = this.projectService.addDeveloperToProject(projectId, developerCredentials);
+            return new ResponseEntity<>(developer, HttpStatus.CREATED);
+        }
+        catch(RuntimeException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage(), e);
+        }
     }
 }
--- src/main/java/com/example/projectmanager/services/ProjectService.java
+++ src/main/java/com/example/projectmanager/services/ProjectService.java
@@ -3,6 +3,8 @@ package com.example.projectmanager.services;
 import com.example.projectmanager.entities.Developer;
 import com.example.projectmanager.entities.Project;
 import com.example.projectmanager.entities.User;
+import com.example.projectmanager.exceptions.ProjectNotFoundException;
+import com.example.projectmanager.exceptions.UserAlreadyPresentException;
 import com.example.projectmanager.exceptions.UserNotFoundException;
 import com.example.projectmanager.repositories.DeveloperRepository;
 import com.example.projectmanager.repositories.ProjectRepository;
@@ -13,37 +15,40 @@ import com.example.projectmanager.utils.Specialization;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import java.util.*;
 
 @Service
 public class ProjectService {
     private final ProjectRepository projectRepository;
     private final UserRepository userRepository;
+    private final DeveloperRepository developerRepository;
     private final DeveloperService developerService;
 
     @Autowired
     public ProjectService(ProjectRepository projectRepository,
                           DeveloperService developerService,
-                          UserRepository userRepository) {
+                          UserRepository userRepository,
+                          DeveloperRepository developerRepository) {
         this.projectRepository = projectRepository;
         this.developerService = developerService;
         this.userRepository = userRepository;
+        this.developerRepository = developerRepository;
     }
 
-
     public Project addNewProject(ProjectCredentials projectCredentials) {
-        String name = projectCredentials.getName();
-        List<DeveloperCredentials> users = projectCredentials.getUsers();
+        String name = projectCredentials.name();
+        List<DeveloperCredentials> users = projectCredentials.users();
         List<Developer> developers = new ArrayList<>();
         for (DeveloperCredentials userSpec : users) {
-            String userId = userSpec.getUserId();
-            Specialization spec = userSpec.getSpecialization();
+            Long userId = userSpec.userId();
+            Specialization spec = userSpec.specialization();
             Optional<User> userOptional = userRepository.findById(userId);
             if (userOptional.isPresent()) {
                 User user = userOptional.get();
                 developers.add(new Developer(user, spec));
             }
-            else throw new UserNotFoundException("User doesnt exist");
+            else throw new UserNotFoundException("User not found");
         }
 
         Project project = new Project(name);
@@ -51,7 +56,7 @@ public class ProjectService {
         projectRepository.save(project);
         for (Developer developer : developers) {
             developer.setProject(project);
-            developerService.createDeveloper(developer);
+            this.developerRepository.save(developer);
         }
         return project;
     }
@@ -62,5 +67,43 @@ public class ProjectService {
 
     public List<Project> getAllProjects() {
         return this.projectRepository.findAll();
+    }
+
+    public Project getProjectById(Long id) {
+        Optional<Project> projectOptional = this.projectRepository.findById(id);
+        if (projectOptional.isPresent()) {
+            return projectOptional.get();
+        }
+        else {
+            throw new ProjectNotFoundException("Project not found");
+        }
+    }
+
+    public List<User> getUsersInProject(Long projectId) {
+        return this.userRepository.findAllByProjectId(projectId);
+    }
+
+    public Boolean isUserInProject(Long projectId, User user) {
+        List<User> usersInProject = getUsersInProject(projectId);
+        return usersInProject.contains(user);
+    }
+
+    public Developer addDeveloperToProject(Long projectId, DeveloperCredentials developerCredentials) {
+        Optional<User> user = this.userRepository.findById(developerCredentials.userId());
+        Optional<Project> project = this.projectRepository.findById(projectId);
+        Specialization specialization = developerCredentials.specialization();
+        if (user.isPresent() && project.isPresent()) {
+            if (!isUserInProject(projectId, user.get())) {
+                Developer developer = new Developer(user.get(), project.get(), specialization);
+                return this.developerRepository.save(developer);
+            }
+            else {
+                throw new UserAlreadyPresentException("User already present in the project");
+            }
+        }
+        else {
+            if (user.isEmpty()) throw new UserNotFoundException("User not found");
+            else throw new ProjectNotFoundException("Project not found");
+        }
     }
 }
