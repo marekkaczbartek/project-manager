5d8fdc5081051f52d2297c21572ecd9c8f012cc3
marekkaczbartek <bartek.kaczmarek03@gmail.com>
2023-11-07 02:01:50 +0100

    added quaring projects by user

--- src/main/java/com/example/projectmanager/repositories/ProjectRepository.java
+++ src/main/java/com/example/projectmanager/repositories/ProjectRepository.java
@@ -2,6 +2,12 @@ package com.example.projectmanager.repositories;
 
 import com.example.projectmanager.entities.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ProjectRepository extends JpaRepository<Project, Long> {
+    @Query("SELECT d.project FROM Developer d WHERE d.user.id = :userId")
+    List<Project> findAllByUserId(@Param("userId") Long userId);
 }
