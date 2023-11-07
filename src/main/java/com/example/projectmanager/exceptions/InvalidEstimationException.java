49f70d293087fb279fadad8ad31612921ea31ae2
marekkaczbartek <bartek.kaczmarek03@gmail.com>
2023-11-07 02:21:28 +0100

    added invalid estimation exception

---
+++ src/main/java/com/example/projectmanager/exceptions/InvalidEstimationException.java
@@ -0,0 +1,7 @@
+package com.example.projectmanager.exceptions;
+
+public class InvalidEstimationException extends RuntimeException {
+    public InvalidEstimationException(String message) {
+        super(message);
+    }
+}
